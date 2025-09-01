bd2f0c0283eda20d761e443ec014e55c450bc749
Ivan-Huang17 <hyf2769408204@163.com>
2025-09-01 05:41:32 +0000

    3

--- 快速查找 .cpp
+++ 快速查找 .cpp
@@ -6,7 +6,7 @@ void swap(int& x,int& y){
 	x=y;
 	y=temp;
 }
-void partition(vector<int>& arr,int low,int high){
+int partition(vector<int>& arr,int low,int high){
 	int temp=arr[low];
 	while(low<high){
 		while(arr[high]>temp){
@@ -20,9 +20,24 @@ void partition(vector<int>& arr,int low,int high){
 		swap(arr[low],arr[high])
 	}
 	arr[low]=temp;
+	return low;
 }
-void QuickSort(vector<int>& arr){
+void QuickSort(vector<int>& arr,int low,int,high){
 	if(low<high){
-		
+		int address=partition(arr,low,high);
+		QuickSort(arr,low,address);
+		QuickSort(arr,address,high);
+
 	}
+}
+int main(){
+	vector<int> nums={5,2,3,1};
+	QuickSort(arr,0,nums.size());
+	cout<<"排序后的数组是："；
+	for(int i=0;i<nums.size());
+	{
+		cout<<nums[i]<<",";
+
+	}
+	return 0;
 }
